2e4b6fc1dd79961d9af8527641f0652546a3a04c
Patryk Stanek <p.stanek3@gmail.com>
2019-03-21 17:27:41 +0100

    first commit

---
+++ modules/server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var colors = require('colors');
+
+var handlers = require('./handlers');
+
+function start() {
+    function onRequest(request, response) {
+        console.log('Odebrano zapytanie.'.yellow);
+        console.log('Zapytanie ' + request.url + ' odebrane.');
+
+        switch (request.url) {
+            case '/css/style.css':
+                handlers.style(response);
+            case '/': //case ten i poniższy wywołuje te same zdarzenia
+            case '/start':
+                handlers.welcome(request, response);
+                break;
+            case '/upload':
+                handlers.upload(request, response);
+                break;
+            case '/show':
+                handlers.show(request, response);
+                break;
+            default:
+                handlers.error(request, response);
+        }
+    }
+
+    http.createServer(onRequest).listen(9000);
+
+    console.log('Uruchomiono serwer!'.green);
+}
+
+exports.start = start;
